63328c88bc061b88561087802113082edc6b1f74
misusonu18 <mishal@freshbits.in>
2021-01-15 17:37:36 +0530

    route: laravel and livewire url

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\NestedController;
+use App\Http\Livewire\NestedComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [NestedController::class, 'index']);
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/laravel', [NestedController::class, 'index'])->name('laravel_nested');
+Route::get('/livewire', NestedComponent::class)->name('livewire_nested');
